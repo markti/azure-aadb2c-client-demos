6a118600daf0427342a34c9ee839648aee1839fb
markti <markti@outlook.com>
2022-06-24 15:54:44 -0400

    add clean xamarin app

---
+++ clients/xamarin/App3/App3/Features/LogOn/IParentWindowLocatorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace App3.Features.LogOn
+{
+    /// <summary>
+    /// Simple platform specific service that is responsible for locating a 
+    /// </summary>
+    public interface IParentWindowLocatorService
+    {
+        object GetCurrentParentWindow();
+    }
+}
+
---
+++ clients/xamarin/App3/App3/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using App3.ViewModels;
+
+namespace App3.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
